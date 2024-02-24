2195d357cc253bf1ab3ec73b16dfe98d7db9ca8b
LouziAmine <louzi.amine.pro@gmail.com>
2024-02-24 01:04:25 +0100

    splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fitness-for-men';
-  imageUrl: string = 'assets/image/www.PNG';
 
 
-  textInput: string = '';
-
-  // onInputChange(): void {
-  //   // Display an alert with the current text input value
-  //   alert('Text input value: ' + this.textInput);
-  // }
-
-  // goToNextStep(): void {
-  //   // Add logic to navigate to the next step
-  //   //console.log('Moving to the next step...');
-  //   // You can add router navigation logic here if you're using Angular Router
-  //
-  //   alert('Text input value: ' + this.textInput);
-  // }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashscreenComponent
   ],
   imports: [
     BrowserModule,
