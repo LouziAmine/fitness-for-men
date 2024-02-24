909aefe33e4ab8b3ef985d85aff590a7ffecb1f1
LouziAmine <louzi.amine.pro@gmail.com>
2024-02-24 01:03:53 +0100

    splashscreen

---
+++ src/app/splashscreen/splashscreen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen',
+  templateUrl: './splashscreen.component.html',
+  styleUrl: './splashscreen.component.sass'
+})
+export class SplashscreenComponent {
+
+}
